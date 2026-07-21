874da28b16aa2f2249aac5a5f83f7bf98e5c0cee
naimaabdi0239s-spec <naima.abdi0239s@gmail.com>
2026-07-21 19:31:13 +0300

    socials links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -725,10 +725,10 @@ function Footer() {
           </p>
         </div>
         <div className="flex flex-col sm:flex-row gap-4 text-[13.5px]" style={{ color: NAVY }}>
-          <a href="mailto:Techsupportforeveryone@gmail.com" className="inline-flex items-center gap-2 hover:opacity-70">
+          <a href="mailto:techsupportforeveryone@gmail.com" className="inline-flex items-center gap-2 hover:opacity-70">
             <Mail className="w-4 h-4" style={{ color: BLUE }} /> Techsupportforeveryone@gmail.com
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noreferrer" className="inline-flex items-center gap-2 hover:opacity-70">
+          <a href="https://www.instagram.com/techsupportforeveryone/" target="_blank" rel="noreferrer" className="inline-flex items-center gap-2 hover:opacity-70">
             <Instagram className="w-4 h-4" style={{ color: BLUE }} /> @techsupportforeveryone
           </a>
         </div>
